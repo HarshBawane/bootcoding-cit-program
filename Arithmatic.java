d928c8906035850a72eaeade94aed30c28892bbf
HarshBawane <144712721+HarshBawane@users.noreply.github.com>
2023-09-12 16:06:48 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,17 @@
+public class Arithmatic
+{
+ public static void main(String args[])
+  { 
+    int a=88,b=12;
+	int add,sub,mul,div;
+	add=a+b;
+	sub=a-b;
+	mul=a*b;
+	div=a/b;
+	System.out.println("Addition:"+add);
+	System.out.println("Substraction:"+sub);
+	System.out.println("Multiplication:"+mul);
+    System.out.println("Division:"+div);	
+  }
+}  
+   
---
+++ Ifelse.java
@@ -0,0 +1,11 @@
+public class Ifelse{
+public static void main(String args[]){
+int a=30,b=20,c=10;
+if(a>b && a>c){
+System.out.println("a is largest no.");
+}else{
+System.out.println("a is smallest no.");
+}
+}
+}
+
